6958c451d3dfdd8cdcc55c7dfa19a29ccace39fb
Fredrick Mwangi <berrymundia@gmail.com>
2025-04-08 09:13:41 +0000

    Add database models and populate database

---
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -0,0 +1,37 @@
+# Add the newly created app to the INSTALLED_APPS list
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'octofit_tracker_app',
+    'rest_framework',
+    'corsheaders',
+    'octofit_tracker',
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'djongo',
+        'NAME': 'octofit_db',
+        'HOST': 'localhost',
+        'PORT': 27017,
+    }
+}
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_METHODS = [
+    'GET',
+    'POST',
+    'PUT',
+    'PATCH',
+    'DELETE',
+    'OPTIONS',
+]
+CORS_ALLOW_HEADERS = [
+    '*',
+]
+
+ALLOWED_HOSTS = ['*']
